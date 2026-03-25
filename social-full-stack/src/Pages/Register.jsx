c33744ed3f7344f9f82b41c7f75f22d20286b1a6
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-25 10:10:10 -0400

    added userclient

--- social-full-stack/src/Pages/Register.jsx
+++ social-full-stack/src/Pages/Register.jsx
@@ -9,18 +9,44 @@ const [form, setForm] = useState({
     password: ''
 })
 
+//its eventhandlerfunction ,userc
+const handleChange = (e) => {
+    setForm({
+        ...form,
+        //updating either uname email or password
+        [e.target.name]: e.target.value
+    })
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(form)
+
+    //send data to our backend
+
+    //teke the token and store it locally
+
+    //save some user data in our state
+
+    //take the user to different page
+
+}
+
     return(
         <div>
             <h1>Register Page</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="username">User Name</label>
-                <input value={form.username} id="username" name = "username" type="text" required />
+                <input value={form.username} 
+                onChange={handleChange}
+                id="username" name = "username" type="text" required />
 
                 <label htmlFor="email">Email</label>
-                <input id="email" name = "email" type="email" required />
+                <input value={form.email} onChange={handleChange} id="email" name = "email" type="email" required />
 
                 <label htmlFor="password">Password</label>
-                <input id="password" name = "password" type="password" required />
+                <input value={form.password} onChange={handleChange} id="password" name = "password" type="password" required />
 
                 <button>Register</button>
             </form>
