aee5937ff4cc699c96919d111d691af12bc34ec8
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-25 09:55:33 -0400

    created useState

--- social-full-stack/src/Pages/Register.jsx
+++ social-full-stack/src/Pages/Register.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 function Register(){
+
+    //properties here should match with backend schema
+const [form, setForm] = useState({
+    username: '',
+    email:'',
+    password: ''
+})
+
     return(
         <div>
             <h1>Register Page</h1>
             <form>
                 <label htmlFor="username">User Name</label>
-                <input id="username" name = "username" type="text" required />
+                <input value={form.username} id="username" name = "username" type="text" required />
 
                 <label htmlFor="email">Email</label>
                 <input id="email" name = "email" type="email" required />
